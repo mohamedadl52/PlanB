58593afd140283c1dea7d58207aa8fdd2dd69945
mohamedadl52 <65096134+mohamedadl52@users.noreply.github.com>
2024-09-09 17:04:06 -0700

    hgashjbadc

--- src/veiws/HOme.vue
+++ src/veiws/HOme.vue
@@ -1,32 +1,97 @@
 <template>
+    <p class="fixed border  bg-black z-10 w-full p-2 text-white bottom-0 left-0">
+        <font-awesome-icon class="text-2xl pointer" :icon="['fab', 'instagram']"></font-awesome-icon>
+
+          <span> icon</span>
+          <span>
+              plann_b_team
+            </span>
+        </p> 
     <div style= "background: linear-gradient(151deg, rgb(0 0 0 / 90%) 61%, rgb(165 34 187))" class="w-auto h-screen  flex justify-center items-center">
-     <div class=" uppercase text-white ">
-        <p style="text-decoration-color : rgb(165 34 187) ,  text-decoration-thickness: 18px " class="underline  text-9xl mb-10" >Plan B</p> 
+   
+        <div class=" uppercase text-white ">
+        <p style="text-decoration-color : rgb(165 34 187) ;  text-decoration-thickness: 18px " class="underline  text-9xl mb-10" >Plan B</p> 
        <p class="text-4xl text-center">لتنظيم المناسبات والفعاليات والبازرات</p>
-      <p>
-        <span> icon</span>
-        <span>
-            plann_b_team
-        </span>
-      </p>   
+         
     </div>
     </div>
     <div style= "background: linear-gradient(151deg, rgb(0 0 0 / 90%) 61%, rgb(165 34 187))" class="w-auto h-screen  p-10">
      <div class=" uppercase text-white ">
-        <p class=" border p-2 border-2 border-white">
+        <p class="  p-2 border-2 border-white">
             plan b
         </p>
 
         <div>
-            <p class="text-right">نبذه عن الشركة </p>
-            <p class="w-64">شركة بالن بي لتنظيم المناسبات والفعاليات والبازارات هي
+
+          
+           <div>
+            <p style="text-decoration-color : #a522bb ;  text-decoration-thickness: 15px " class="text-right underline text-2xl leading-10">نبذه عن الشركة </p>
+             <div class="flex justify-start items-center">
+                <img class="w-1/2 h-auto" src="@/assets/logo.jpg" alt="">
+            <p class=" absolute bg-purple-600 text-xl right-20 p-10 w-1/2 ">شركة بالن بي لتنظيم المناسبات والفعاليات والبازارات هي
 شركة سودانية رائدة في مجال تنظيم وإدارة الفعاليات
 المتنوعة. تأسست الشركة لتلبية الحاجة المتزايدة لتنظيم
 فعاليات عالية الجودة ومبتكرة في السودان، ومصر، والمملكة
 العربية السعودية.</p>
+             </div>
+           </div>
+           
         </div>
     </div>
     </div>
+    <div style= "background: linear-gradient(151deg, rgb(0 0 0 / 90%) 61%, rgb(165 34 187))" class="w-auto h-screen  p-10 text-white">
+            <p style="text-decoration-color : #a522bb ;  text-decoration-thickness: 15px " class="text-right underline text-2xl leading-10"> رسالتنا </p>
+             <div class="flex mt-20 justify-start flex-row-reverse gap-10 items-end">
+ <div class="w-1/4">
+    <img class="w-full h-auto" src="@/assets/logo.jpg" alt="">
+
+ </div>
+                <p class="  bg-purple-600 text-xl right-20 p-10 w-2/3 ">شركة بالن بي لتنظيم المناسبات والفعاليات والبازارات هي
+شركة سودانية رائدة في مجال تنظيم وإدارة الفعاليات
+المتنوعة. تأسست الشركة لتلبية الحاجة المتزايدة لتنظيم
+فعاليات عالية الجودة ومبتكرة في السودان، ومصر، والمملكة
+العربية السعودية.</p>
+             </div>
+           </div>
+          
+    <div style= "background: linear-gradient(151deg, rgb(0 0 0 / 90%) 61%, rgb(165 34 187))" class="w-auto h-screen  p-10 text-white">
+        <div class="flex justify-start flex-row gap-10 items-end">
+            <div class="w-1/2">
+                <img class="w-full h-auto" src="@/assets/logo.jpg" alt="">
+                
+            </div>
+              <div>
+                <p style="text-decoration-color : #a522bb ;  text-decoration-thickness: 15px " class="text-right underline text-2xl  mb-10"> خدماتنا </p>
+            
+                <div class=" text-right   text-lg  p-10  ">
+                   <div class="border-r-8 px-8 border-white">
+                    <p class="text-2xl font-bold">
+                        
+                        :  تنظيم المؤتمرات والمعارض  
+                         </p>
+                         <p>
+                             <span>*</span>
+                             <span>
+                                توفير أحدث التقنيات والخدمات المؤتمرات والمعارض المحلية والدولية
+                             </span>
+                             
+                         </p>
+                   </div>
+                   <div class="border-r-8 mt-8 px-8 border-white">
+                    <p>
+                        
+                        :  تنظيم المؤتمرات والمعارض  
+                         </p>
+                         <p>
+                            توفير أحدث التقنيات والخدمات المؤتمرات والمعارض المحلية والدولية
+    
+                         </p>
+                   </div>
+                 </div>
+              </div>
+             </div>
+           </div>
+
 </template>
 
 <style  scoped>
